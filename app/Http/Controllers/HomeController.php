e2f645c495284311ee51d87bff1f08d91f73562d
itokiana <itokianarajoelison@gmail.com>
2022-03-14 07:15:54 +0300

    🐛 FIX:  Modification du systeme d'affichage du total caisse

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,12 @@ class HomeController extends Controller
     public function index()
     {
         $operation_caisses = OperationCaisse::get();
-        $total_caisse = OperationCaisse::sum('total_operation');
+        $total_retrait = OperationCaisse::where('type_operation','retrait')->sum('total_operation');
+        $total_depot = OperationCaisse::where('type_operation','depot')->sum('total_operation');
+        $total_remise_bank = OperationCaisse::where('type_operation','remise_bank')->sum('total_operation');
+
+        $total_caisse = ($total_depot + $total_remise_bank) - $total_retrait;
+
 
         return view('home',[
             'operation_caisses' => $operation_caisses,
